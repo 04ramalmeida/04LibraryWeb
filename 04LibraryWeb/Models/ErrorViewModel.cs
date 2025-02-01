6bf9f1b3392f024b5f7ef228b2490760022aee73
Joao Almeida <176084410+04ramalmeida@users.noreply.github.com>
2025-02-01 15:32:31 +0000

    Initi commit

---
+++ 04LibraryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _04LibraryWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
